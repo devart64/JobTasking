c6877824a5e938ee16dfc0c4250e9e3d4c7a19b8
devart64 <stephendupre64@gmail.com>
2021-09-26 16:30:50 +0200

    ajout images

--- assets/components/home/home.js
+++ assets/components/home/home.js
@@ -7,13 +7,18 @@ export default class Home extends React.Component{
         super(props);
     }
 
+    handleClick = (event) => {
+        // il faut passer par les states pour changer l'état de la vue qu click sur le bouton
+        // au click on affichera la liste des tâche restantes de la journée.
+    }
+
     render() {
         return (
-            <div id="row" className='imageHomebackground'>
+            <div id="row" className='container_home_page'>
                 <img src={imageCleaner} alt='image home page' width='100%' height='100%'/>
                 <h1 className='text-center'>Welcome to JobCleaning !</h1>
                 <div className="group-button text-center mt-2">
-                    <button className='btn btn-primary mt-2'>Voir les tâches restantes du jour</button>
+                    <button className='btn btn-primary mt-2' onClick={this.handleClick}>Voir les tâches restantes du jour</button>
                 </div>
             </div>
         );
